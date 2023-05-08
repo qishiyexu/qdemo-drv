dcad89d01ff08fe932f42f928ff78ce57f14b511
chenzhao <chenz9@chinatelecom.cn>
2023-05-08 08:57:50 +0800

    add

--- src/qdemo-drv.c
+++ src/qdemo-drv.c
@@ -17,6 +17,7 @@
 
 
 #define DRV_NAME 		"qdemo"
+#define DRV_FILE_FMT 	DRV_NAME"%d"
 #define DRV_VERSION		"0.1"
 #define DOMAIN          "[qdemo]"
 
@@ -59,135 +60,136 @@ static struct pci_device_id qdemo_id_table[] = {
     { 0 },
 };
 
-static int ivpci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
+static struct file_operations qdemo_ops = {
+    .owner          = THIS_MODULE,
+    // .open           = ivpci_open,
+    // .mmap           = ivpci_mmap,
+    // .unlocked_ioctl = ivpci_ioctl,
+    // .read           = ivpci_read,
+    // .write          = ivpci_write,
+    // .llseek         = ivpci_lseek,
+    // .release        = ivpci_release,
+};
+
+static int qdemo_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
     int ret;
-    struct ivpci_private *ivpci_dev;
     dev_t devno;
 
-    dev_info(&pdev->dev, PFX "probing for device: %s\n", pci_name(pdev));
-
-    if (g_ivpci_count >= g_max_devices) {
-        dev_err(&pdev->dev, PFX "reach the maxinum number of devices, " \
-                "please adapt the `g_max_devices` value, reload the driver\n");
-        ret = -1;
-        goto out;
-    }
+    dev_info(&pdev->dev, DOMAIN "probing for device: %s\n", pci_name(pdev));
 
     // Active pci device by setting some registers.
     ret = pci_enable_device(pdev);
     if (ret < 0) {
-        dev_err(&pdev->dev, PFX "unable to enable device: %d\n", ret);
+        dev_err(&pdev->dev, DOMAIN "unable to enable device: %d\n", ret);
         goto out;
     }
 
     /* Reserved PCI I/O and memory resources for this device */
     ret = pci_request_regions(pdev, DRV_NAME);
     if (ret < 0) {
-        dev_err(&pdev->dev, PFX "unable to reserve resources: %d\n", ret);
+        dev_err(&pdev->dev, DOMAIN "unable to reserve resources: %d\n", ret);
         goto disable_device;
     }
 
-    ivpci_dev = ivpci_get_private();
-    BUG_ON(ivpci_dev == NULL);
+    BUG_ON(g_qdemo_dev == NULL);
 
     // Get revision id
-    pci_read_config_byte(pdev, PCI_REVISION_ID, &ivpci_dev->revision);
+    pci_read_config_byte(pdev, PCI_REVISION_ID, &g_qdemo_dev->revision);
 
-    dev_info(&pdev->dev, PFX "device %d:%d, revision: %d\n", g_ivpci_major,
-            ivpci_dev->minor, ivpci_dev->revision);
+    dev_info(&pdev->dev, DOMAIN "device %d:%d, revision: %d\n", g_qdemo_major,
+            g_qdemo_dev->minor, g_qdemo_dev->revision);
 
     /* Pysical address of BAR0, BAR1, BAR2 */
-    ivpci_dev->bar0_addr = pci_resource_start(pdev, 0);
-    ivpci_dev->bar0_len = pci_resource_len(pdev, 0);
-    ivpci_dev->bar1_addr = pci_resource_start(pdev, 1);
-    ivpci_dev->bar1_len = pci_resource_len(pdev, 1);
-    ivpci_dev->bar2_addr = pci_resource_start(pdev, 2);
-    ivpci_dev->bar2_len = pci_resource_len(pdev, 2);
+    g_qdemo_dev->bar0_addr = pci_resource_start(pdev, 0);
+    g_qdemo_dev->bar0_len = pci_resource_len(pdev, 0);
+    g_qdemo_dev->bar1_addr = pci_resource_start(pdev, 1);
+    g_qdemo_dev->bar1_len = pci_resource_len(pdev, 1);
+    g_qdemo_dev->bar2_addr = pci_resource_start(pdev, 2);
+    g_qdemo_dev->bar2_len = pci_resource_len(pdev, 2);
 
     // TODO:: print with 32 bit/64 bit?
-    dev_info(&pdev->dev, PFX "BAR0: 0x%0llx, %d\n", ivpci_dev->bar0_addr,
-            ivpci_dev->bar0_len);
-    dev_info(&pdev->dev, PFX "BAR1: 0x%0llx, %d\n", ivpci_dev->bar1_addr,
-            ivpci_dev->bar1_len);
-    dev_info(&pdev->dev, PFX "BAR2: 0x%0llx, %d\n", ivpci_dev->bar2_addr,
-            ivpci_dev->bar2_len);
+    dev_info(&pdev->dev, DOMAIN "BAR0: 0x%0llx, %d\n", g_qdemo_dev->bar0_addr,
+            g_qdemo_dev->bar0_len);
+    dev_info(&pdev->dev, DOMAIN "BAR1: 0x%0llx, %d\n", g_qdemo_dev->bar1_addr,
+            g_qdemo_dev->bar1_len);
+    dev_info(&pdev->dev, DOMAIN "BAR2: 0x%0llx, %d\n", g_qdemo_dev->bar2_addr,
+            g_qdemo_dev->bar2_len);
 
     // Map bar0(register) from physical address to virtual address.
-    ivpci_dev->regs_addr = ioremap(ivpci_dev->bar0_addr, ivpci_dev->bar0_len);
-    if (!ivpci_dev->regs_addr) {
-        dev_err(&pdev->dev, PFX "unable to ioremap bar0, size: %d\n",
-                ivpci_dev->bar0_len);
+    g_qdemo_dev->regs_addr = ioremap(g_qdemo_dev->bar0_addr, g_qdemo_dev->bar0_len);
+    if (!g_qdemo_dev->regs_addr) {
+        dev_err(&pdev->dev, DOMAIN "unable to ioremap bar0, size: %d\n",
+                g_qdemo_dev->bar0_len);
         goto release_regions;
     }
 
     // Bar2 is the share memory address
-    ivpci_dev->base_addr = ioremap(ivpci_dev->bar2_addr, ivpci_dev->bar2_len);
-    if (!ivpci_dev->base_addr) {
-        dev_err(&pdev->dev, PFX "unable to ioremap bar2, size: %d\n",
-                ivpci_dev->bar2_len);
+    g_qdemo_dev->base_addr = ioremap(g_qdemo_dev->bar2_addr, g_qdemo_dev->bar2_len);
+    if (!g_qdemo_dev->base_addr) {
+        dev_err(&pdev->dev, DOMAIN "unable to ioremap bar2, size: %d\n",
+                g_qdemo_dev->bar2_len);
         goto iounmap_bar0;
     }
-    dev_info(&pdev->dev, PFX "BAR2 map: %p\n", ivpci_dev->base_addr);
+    dev_info(&pdev->dev, DOMAIN "BAR2 map: %p\n", g_qdemo_dev->base_addr);
 
     /*
      * Create character device file.
      */
-    cdev_init(&ivpci_dev->cdev, &ivpci_ops);
-    ivpci_dev->cdev.owner = THIS_MODULE;
+    cdev_init(&g_qdemo_dev->cdev, &qdemo_ops);
+    g_qdemo_dev->cdev.owner = THIS_MODULE;
 
-    devno = MKDEV(g_ivpci_major, ivpci_dev->minor);
-    ret = cdev_add(&ivpci_dev->cdev, devno, 1);
+    devno = MKDEV(g_qdemo_major, g_qdemo_dev->minor);
+    ret = cdev_add(&g_qdemo_dev->cdev, devno, 1);
     if (ret < 0) {
-        dev_err(&pdev->dev, PFX "unable to add chrdev %d:%d to system: %d\n",
-                g_ivpci_major, ivpci_dev->minor, ret);
+        dev_err(&pdev->dev, DOMAIN "unable to add chrdev %d:%d to system: %d\n",
+                g_qdemo_major, g_qdemo_dev->minor, ret);
         goto iounmap_bar2;
     }
 
-    if (device_create(g_ivpci_class, NULL, devno, NULL, DRV_FILE_FMT,
-                ivpci_dev->minor) == NULL)
+    if (device_create(g_qdemo_class, NULL, devno, NULL, DRV_FILE_FMT,
+                g_qdemo_dev->minor) == NULL)
     {
-        dev_err(&pdev->dev, PFX "unable to create device file: %d:%d\n",
-                g_ivpci_major, ivpci_dev->minor);
+        dev_err(&pdev->dev, DOMAIN "unable to create device file: %d:%d\n",
+                g_qdemo_major, g_qdemo_dev->minor);
         goto delete_chrdev;
     }
 
-    ivpci_dev->dev = pdev;
-    pci_set_drvdata(pdev, ivpci_dev);
+    g_qdemo_dev->dev = pdev;
+    pci_set_drvdata(pdev, g_qdemo_dev);
 
-    if (ivpci_dev->revision == 1) {
-        /* Only process the MSI-X interrupt. */
-        ivpci_dev->ivposition = ioread32(ivpci_dev->regs_addr + IVPOSITION_OFF);
+    // if (g_qdemo_dev->revision == 1) {
+    //     /* Only process the MSI-X interrupt. */
+    //     g_qdemo_dev->ivposition = ioread32(g_qdemo_dev->regs_addr + IVPOSITION_OFF);
 
-        dev_info(&pdev->dev, PFX "device ivposition: %u, MSI-X: %s\n",
-                ivpci_dev->ivposition,
-                (ivpci_dev->ivposition == 0) ? "no": "yes");
+    //     dev_info(&pdev->dev, DOMAIN "device ivposition: %u, MSI-X: %s\n",
+    //             g_qdemo_dev->ivposition,
+    //             (g_qdemo_dev->ivposition == 0) ? "no": "yes");
 
-        if (ivpci_dev->ivposition != 0) {
-            ret = ivpci_request_msix_vectors(ivpci_dev, 4);
-            if (ret != 0) {
-                goto destroy_device;
-            }
-        }
-    }
+    //     if (g_qdemo_dev->ivposition != 0) {
+    //         ret = ivpci_request_msix_vectors(g_qdemo_dev, 4);
+    //         if (ret != 0) {
+    //             goto destroy_device;
+    //         }
+    //     }
+    // }
 
-    g_ivpci_count++;
-    dev_info(&pdev->dev, PFX "device probed: %s\n", pci_name(pdev));
+    dev_info(&pdev->dev, DOMAIN "device probed: %s\n", pci_name(pdev));
     return 0;
 
-destroy_device:
-    devno = MKDEV(g_ivpci_major, ivpci_dev->minor);
-    device_destroy(g_ivpci_class, devno);
-    ivpci_dev->dev = NULL;
+// destroy_device:
+//     devno = MKDEV(g_qdemo_major, g_qdemo_dev->minor);
+//     device_destroy(g_qdemo_class, devno);
+//     g_qdemo_dev->dev = NULL;
 
 delete_chrdev:
-    cdev_del(&ivpci_dev->cdev);
+    cdev_del(&g_qdemo_dev->cdev);
 
 iounmap_bar2:
-    iounmap(ivpci_dev->base_addr);
+    iounmap(g_qdemo_dev->base_addr);
 
 iounmap_bar0:
-    iounmap(ivpci_dev->regs_addr);
+    iounmap(g_qdemo_dev->regs_addr);
 
 release_regions:
     pci_release_regions(pdev);
@@ -201,27 +203,23 @@ out:
 }
 
 
-static void ivpci_remove(struct pci_dev *pdev)
+static void qdemo_remove(struct pci_dev *pdev)
 {
     int devno;
-    struct ivpci_private *ivpci_dev;
 
-    dev_info(&pdev->dev, PFX "removing ivshmem device: %s\n", pci_name(pdev));
+    dev_info(&pdev->dev, DOMAIN "removing ivshmem device: %s\n", pci_name(pdev));
 
-    ivpci_dev = pci_get_drvdata(pdev);
-    BUG_ON(ivpci_dev == NULL);
+    // ivpci_free_msix_vectors(g_qdemo_dev);
 
-    ivpci_free_msix_vectors(ivpci_dev);
+    g_qdemo_dev->dev = NULL;
 
-    ivpci_dev->dev = NULL;
+    devno = MKDEV(g_qdemo_major, g_qdemo_dev->minor);
+    device_destroy(g_qdemo_class, devno);
 
-    devno = MKDEV(g_ivpci_major, ivpci_dev->minor);
-    device_destroy(g_ivpci_class, devno);
+    cdev_del(&g_qdemo_dev->cdev);
 
-    cdev_del(&ivpci_dev->cdev);
-
-    iounmap(ivpci_dev->base_addr);
-    iounmap(ivpci_dev->regs_addr);
+    iounmap(g_qdemo_dev->base_addr);
+    iounmap(g_qdemo_dev->regs_addr);
 
     pci_release_regions(pdev);
     pci_disable_device(pdev);
@@ -237,16 +235,16 @@ static struct pci_driver qdemo_driver = {
 
 static int __init qdemo_init(void)
 {
-    int ret, i, minor;
+    int ret;
 
     pr_info( DOMAIN "qdemo init\n");
     ret = alloc_chrdev_region(&g_qdemo_devno, 0, g_max_devices, DRV_NAME);
     if (ret < 0) {
-        pr_err(PFX "unable to allocate major number: %d\n", ret);
+        pr_err(DOMAIN "unable to allocate major number: %d\n", ret);
         goto out;
     }
 
-    g_qdemo_dev = kzalloc(sizeof(struct g_qdemo_dev),
+    g_qdemo_dev = kzalloc(sizeof(g_qdemo_dev),
             GFP_KERNEL);
     if (g_qdemo_dev == NULL) {
         goto unregister_chrdev;
@@ -256,16 +254,16 @@ static int __init qdemo_init(void)
 
     g_qdemo_class = class_create(THIS_MODULE, DRV_NAME);
     if (g_qdemo_class == NULL) {
-        pr_err(PFX "unable to create the struct class\n");
+        pr_err(DOMAIN "unable to create the struct class\n");
         goto free_dev;
     }
 
     g_qdemo_major = MAJOR(g_qdemo_devno);
-    pr_info(PFX "major: %d, minor: %d\n", g_qdemo_major, MINOR(g_qdemo_devno));
+    pr_info(DOMAIN "major: %d, minor: %d\n", g_qdemo_major, MINOR(g_qdemo_devno));
 
     ret = pci_register_driver(&qdemo_driver);
     if (ret < 0) {
-        pr_err(PFX "unable to register driver: %d\n", ret);
+        pr_err(DOMAIN "unable to register driver: %d\n", ret);
         goto destroy_class;
     }
 
@@ -278,7 +276,7 @@ free_dev:
     kfree(g_qdemo_dev);
 
 unregister_chrdev:
-    unregister_chrdev_region(g_ivpci_devno, g_max_devices);
+    unregister_chrdev_region(g_qdemo_devno, g_max_devices);
 
 out:
     return -1;
@@ -297,8 +295,8 @@ static void __exit qdemo_exit(void)
 }
 
 
-module_init(hello_3_init);
-module_exit(hello_3_exit);
+module_init(qdemo_init);
+module_exit(qdemo_exit);
 
 
 MODULE_AUTHOR("qishiyexu2@126.com");
